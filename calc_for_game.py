14c69049cef1721f7c38c43a966a2c0daa3decc8
skmake <matboe0@gmail.com>
2025-01-09 10:15:53 +0100

    Fixed code
    No sussy code

--- calc_for_game.py
+++ calc_for_game.py
@@ -14,3 +14,7 @@ def skibidi_calc(first_num:float,operator:str,second_num:float):
 
 def skibid_calc_2(third_num:float):
     return third_num
+
+def skibid_calc_3(um:float):
+    print("Skibidi pop pop", skibid_calc_2(um)*68, "number of cho")
+    
