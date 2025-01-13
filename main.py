cd1871e14f808463b8e104ec135afd040b47f0a5
Saileeich <109689239+Saileeich@users.noreply.github.com>
2025-01-13 13:39:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,6 @@ from player import Player
 WIDTH, HEIGHT = 400, 400
 FPS = 24
 
-class MySprite(pygame.sprite.Sprite):
-    def __init__(self, image_path, pos):
-        super().__init__()
-        self.image = pygame.image.load(image_path)  # Load the sprite's image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = pos  # Set the sprite's position
-
-    def update(self):
-        # Add logic for movement or behavior here
-        self.rect.x += 1  # Example: Move the sprite to the right
-
 class App:
     def __init__(self):
         self.awake()
